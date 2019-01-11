dfd8516d74f2f6a175b3f1b32e89bae3db822009
wangjch <wangjchm@yonyou.com>
2019-01-11 11:16:50 +0800

    添加参照

--- src/pages/quote/components/index.js
+++ src/pages/quote/components/index.js
@@ -1,94 +1,130 @@
-import React, {Component} from 'react';
-import {actions} from 'mirrorx';
-import {Icon, Loading,Row,Col,Button, FormControl } from 'tinper-bee';
-import Select from 'bee-select';
-
+import React, { Component } from "react";
+import { actions } from "mirrorx";
+import { Icon, Loading, Row, Col, Button, FormControl } from "tinper-bee";
+import Select from "bee-select";
+import RefMultipleTableWithInput  from "ref-multiple-table";
 
 // import Grid from 'components/Grid';
-import Subject from './Subject';
+import Subject from "./Subject";
 
-
-import Part from './Part'
-import QuoteTable from './Quote'
+import Part from "./Part";
+import QuoteTable from "./Quote";
 
 // import 'bee-complex-grid/build/Grid.css';
 // import 'bee-pagination/build/Pagination.css'
 // import 'bee-table/build/Table.css';
 // import 'bee-input-number/build/InputNumber.css';
-import './index.less';
-
-const {Option} = Select;
+import "ref-multiple-table/dist/index.css";
+import "./index.less";
 
 class Quote extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  componentDidMount() {
+    // actions.query.loadList(this.props.queryParam); // 查询默认条件
+  }
 
-    componentDidMount() {
-        // actions.query.loadList(this.props.queryParam); // 查询默认条件
-    }
+  render() {
+    const _this = this;
+    const {
+      subjectObj,
+      subjectModalObj,
+      partObj,
+      quoteIndex,
+      partIndex,
+      quoteList
+    } = _this.props;
 
-
-    render() {
-        const _this = this;
-        const {subjectObj, subjectModalObj, partObj, quoteIndex, partIndex, quoteList} = _this.props;
-
-        return (
-            <div className='quote'>
-                <div className="pro-info">
-                    <Row>
-                        <Col md={4} xs={12} sm={12}>
-                            客户名称：
-                            <Select
-                                defaultValue="lucy"
-                                style={{ width: 200 }}
-                                // onChange={this.handleChange}
-                                >
-                            <Option value="jack">boyuzhou</Option>
-                            <Option value="lucy">renhualiu</Option>
-                            <Option value="yiminghe">yuzhao</Option>
-                            </Select>
-                        </Col>
-                        <Col md={4} xs={12} sm={12}>
-                            设计中心：
-                            <FormControl readOnly />
-                        </Col>
-                        <Col md={4} xs={12} sm={12}>
-                            项目地址：
-                            <FormControl readOnly />
-                        </Col>
-                    </Row>
-                </div>
-                <div>
-                    <div className="btns-row">
-                        <Button colors="primary" size="sm" onClick={() => actions.quote.createQuote()}>创建报价</Button>
-                        <Button colors="primary" size="sm">参考其他项目报价</Button>
-                        <Button colors="primary" size="sm">提交报价</Button>
-                        <Button colors="primary" size="sm">打印报价</Button>
-                    </div>
-                    <Row>
-                        <Col md={4} xs={12} sm={12}>
-                            <div className='create-name'>
-                                <QuoteTable quoteList={quoteList} quoteIndex={quoteIndex}/>
-                            </div>
-                            <div className='create-part'>
-                                <Part partObj={partObj} partIndex={partIndex}/>
-                            </div>
-                        </Col>
-                        <Col md={8} xs={12} sm={12}>
-                            <div className="create-project">
-                                <Subject
-                                    subjectObj={subjectObj}
-                                    subjectModalObj={subjectModalObj}/>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="quote">
+        <div className="pro-info">
+          <Row>
+            <Col md={4} xs={12} sm={12}>
+              客户名称：
+              <RefMultipleTableWithInput
+                placeholder="请选择客户"
+                title={"复杂表格参照"}
+                backdrop={true}
+                disabled={false}
+                multiple={false}
+                strictMode={true}
+                param={{
+                  //url请求参
+                  refCode: "post_level" //test_common||test_grid||test_tree||test_treeTable
+                }}
+                refModelUrl={{
+                  tableBodyUrl: "/iuap_walsin_demo/common-ref/blobRefTreeGrid", //表体请求
+                  refInfo: "/iuap_walsin_demo/common-ref/refInfo" //表头请求
+                }}
+                matchUrl={"/iuap_walsin_demo/common-ref/matchPKRefJSON"}
+                filterUrl={"/iuap_walsin_demo/common-ref/filterRefJSON"}
+                valueField={"refpk"}
+                displayField={"{refname}-{refpk}"}
+                // {...getFieldProps("valueField", {
+                //   initialValue: '{"refname":"高级-T3","refpk":"level5"}',
+                //   rules: [
+                //     {
+                //       message: "请输入姓名",
+                //       pattern: /[^{"refname":"","refpk":""}|{"refpk":"","refname":""}]/
+                //     }
+                //   ]
+                // })}
+              />
+            </Col>
+            <Col md={4} xs={12} sm={12}>
+              设计中心：
+              <FormControl readOnly />
+            </Col>
+            <Col md={4} xs={12} sm={12}>
+              项目地址：
+              <FormControl readOnly />
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <div className="btns-row">
+            <Button
+              colors="primary"
+              size="sm"
+              onClick={() => actions.quote.createQuote()}
+            >
+              创建报价
+            </Button>
+            <Button colors="primary" size="sm">
+              参考其他项目报价
+            </Button>
+            <Button colors="primary" size="sm">
+              提交报价
+            </Button>
+            <Button colors="primary" size="sm">
+              打印报价
+            </Button>
+          </div>
+          <Row>
+            <Col md={4} xs={12} sm={12}>
+              <div className="create-name">
+                <QuoteTable quoteList={quoteList} quoteIndex={quoteIndex} />
+              </div>
+              <div className="create-part">
+                <Part partObj={partObj} partIndex={partIndex} />
+              </div>
+            </Col>
+            <Col md={8} xs={12} sm={12}>
+              <div className="create-project">
+                <Subject
+                  subjectObj={subjectObj}
+                  subjectModalObj={subjectModalObj}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Quote;
